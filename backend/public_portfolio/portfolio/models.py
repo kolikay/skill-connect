bc3e23111c815fcd1ade3c237bd87c0513e329b0
kolikay <kolikay1989@yahoo.com>
2020-07-09 10:50:42 +0100

    working on views

--- backend/public_portfolio/portfolio/models.py
+++ backend/public_portfolio/portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    name        = models.CharField(max_length=122, null=False, blank=False)
+    name        = models.CharField(max_length=122, null=False, blank=False )
     age         = models.IntegerField(default=2, null=True, blank=True)
     email       = models.EmailField(unique=True, null=False, blank=False)
     phone       = models.CharField(max_length=11, null=True, blank=True)
